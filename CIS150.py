e6f836e37ca8a81fd4276bfbdb7daed6cc197a90
dwylie86 <dwylie86@gmail.com>
2025-03-17 11:40:28 -0700

    CIS150 Assignment and sync fix

---
+++ CIS150.py
@@ -0,0 +1,19 @@
+# David Wylie
+# CIS150AB Spring 2025
+# Print Asterisks
+
+def print_asterisks():
+    """
+        Ouputs the text "CIS150" to console using asterisks
+    """
+    print("""
+          *******     *****     *******       *     *******     *******
+          *             *       *           * *     *           *     *
+          *             *       *             *     *****       *     *
+          *             *       *******       *          *      *     *
+          *             *             *       *           *     *     *
+          *             *             *       *          *      *     *
+          *******     *****     *******      ***    *****       *******
+          """ )
+    
+print_asterisks()
